c0b1e24874d7d27851765d88252979f508242fd9
Raksha Soni <raksha.soni.dev@gmail.com>
2026-07-14 14:50:14 +0530

    build screen recorder UI

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Screen Recorder");
+const startBtn = document.getElementById("startBtn");
+
+const pauseBtn = document.getElementById("pauseBtn");
+
+const resumeBtn = document.getElementById("resumeBtn");
+
+const stopBtn = document.getElementById("stopBtn");
+
+const downloadBtn = document.getElementById("downloadBtn");
+
+const previewVideo = document.getElementById("previewVideo");
+
+console.log("UI Ready");
