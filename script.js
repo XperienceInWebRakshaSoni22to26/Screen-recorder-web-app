cfb9bfcbfd7aa4d63eceffc22a49049bc1292b1e
Raksha Soni <raksha.soni.dev@gmail.com>
2026-07-14 15:00:00 +0530

    Add screen capture functionality

--- script.js
+++ script.js
@@ -9,5 +9,33 @@ const stopBtn = document.getElementById("stopBtn");
 const downloadBtn = document.getElementById("downloadBtn");
 
 const previewVideo = document.getElementById("previewVideo");
+let screenStream;
+startBtn.addEventListener("click", startScreenCapture);
+
+async function startScreenCapture() {
+
+    try {
+
+        screenStream = await navigator.mediaDevices.getDisplayMedia({
+            video: true
+        });
+
+        previewVideo.srcObject = screenStream;
+
+        previewVideo.play();
+
+    }
+
+    catch (error) {
+
+        console.error(error);
+
+        alert("Screen sharing was cancelled.");
+
+    }
+
+}
+previewVideo.srcObject = screenStream;
+previewVideo.play();
 
 console.log("UI Ready");
